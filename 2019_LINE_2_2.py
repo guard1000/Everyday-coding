8bb835a0896f71ccd234b00ab625df184500ee3b
guard1000 <cjsdnr885@naver.com>
2018-11-11 03:14:40 +0900

    LINE_2_2-코테용 몇몇개

---
+++ 2019_LINE_2_2.py
@@ -0,0 +1,14 @@
+#tlist=input().split()
+#mypermuatation = itertools.permutations(tlist)
+#mypermuatation = itertools.combinations(tlist)
+#import re    #입력받는 특수문자, 숫자를 없애기 위해 정규식 사용
+#str1 = re.sub('[^A-Za-z]+', '', str1)  #A-Z나 a-z외엔 모두제거
+#str1 = list(str1)             #한글자씩 list로 넣음
+#n, t, m, p = map(int, input().split())
+#l = [[0]*4 for i in range(4)] # 이차원 리스트
+#blist = copy.copy(alist)   #리스트복제
+#mypermutation = itertools.combinations(alist,j+1) #j+1개 조합
+#numbers.remove(max(tmplist, key=lambda x: x % 10))
+# 람다 사용하기!
+#str(max(tmplist, key=lambda x: x % 10))
+#pb= sorted(phone_book, key=lambda x: len(x))
